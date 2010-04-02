4805665abc42f87281a0652d887bcb047d85205b
Mykola Rybak <mykola.rybak@gmail.com>
2010-04-02 13:55:36 +0300

    Command line parsing using optparse.

--- aria2_ctl.py
+++ aria2_ctl.py
@@ -2,31 +2,55 @@ import logging
 import logging.config
 import os
 import os.path
-import sys
 import xmlrpclib
+
+from optparse import OptionParser
+
+def path_for_url(url, base_path):
+    url_split = url.split('://', 1)
+    if len(url_split) == 1:
+        server_path = url_split[0]
+    else:
+        server_path = url_split[1]
+    server_path_components = server_path.split('/')[:-1]
+    return os.path.join(base_path, *server_path_components)
+
+def collect_options(url, options):
+    download_options = {}
+    download_options['remote-time'] = 'true'
+    if options.dest_folder:
+        path = path_for_url(url, options.dest_folder)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        download_options['dir'] = path
+    if options.referer:
+        download_options['referer'] = options.referer
+    if options.cookie:
+        download_options['header'] = 'Cookie:' + options.cookie
+    return download_options
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 logging.config.fileConfig(os.path.join(base_path, 'aria2_ctl.logging.conf'))
 logger = logging.getLogger('aria2_ctl')
 
-def path_for_uri(uri, base_path):
-    uri_split = uri.split('://', 1)
-    if len(uri_split) == 1:
-        server_path = uri_split[0]
-    else:
-        server_path = uri_split[1]
-    server_path_components = server_path.split('/')[:-1]
-    return os.path.join(base_path, *server_path_components)
+parser = OptionParser()
+parser.add_option('-d', '--dest-folder')
+parser.add_option('-i', '--input')
+parser.add_option('--comment')
+parser.add_option('--referer')
+parser.add_option('--cookie')
+(options, args) = parser.parse_args()
 
-if __name__ == '__main__':
-    logger.debug('Arguments: ' + '|'.join(sys.argv))
-    base_path = 't:\\download'
-    if len(sys.argv) == 1:
-        exit(1)
-    uri = sys.argv[1]
-    path = path_for_uri(uri, base_path)
-    if not os.access(path, os.F_OK):
-        os.makedirs(path)
+if len(args) > 0:
+    url_source = (args[0],)
+elif options.input:
+    url_source = open(option.input, 'r')
+else:
+    parser.error('Either -i or url required')
 
-    s = xmlrpclib.ServerProxy('http://localhost:6800/rpc')
-    s.aria2.addUri([uri], {'dir': path, 'remote-time': 'true'})
+s = xmlrpclib.ServerProxy('http://localhost:6800/rpc')
+
+for url in url_source:
+    url_options = collect_options(url, options)
+    logger.debug('Fetching %s with options %s' % (url, url_options))
+    s.aria2.addUri([url], url_options)
